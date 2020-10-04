804849a678bf988d7a8f0324dc04587299975809
TomekLesniak <tomasz.lesniak152@gmail.com>
2020-10-04 19:11:37 +0200

    Update GetGPA and HelpCommand

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -16,10 +16,11 @@ namespace GradeBook.GradeBooks
         public List<Student> Students { get; set; }
         public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeight)
         {
             Name = name;
             Students = new List<Student>();
+            IsWeighted = isWeight;
         }
 
         public void AddStudent(Student student)
@@ -108,20 +109,26 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            int bonus = 0;
+            if(IsWeighted)
+                bonus = (studentType == StudentType.Honors 
+                         || studentType == StudentType.DualEnrolled) ? 1 : 0;
+
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    return 4 + bonus;
                 case 'B':
-                    return 3;
+                    return 3 + bonus;
                 case 'C':
-                    return 2;
+                    return 2 + bonus;
                 case 'D':
-                    return 1;
+                    return 1 + bonus;
                 case 'F':
-                    return 0;
+                    return 0 + bonus;
             }
-            return 0;
+
+            return 0 + bonus;
         }
 
         public virtual void CalculateStatistics()
--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,7 +6,7 @@ namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name) : base(name)
+        public RankedGradeBook(string name, bool isWeight) : base(name, isWeight)
         {
             this.Type = GradeBookType.Ranked;
         }
